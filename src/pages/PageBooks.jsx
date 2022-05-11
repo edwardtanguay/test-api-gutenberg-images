588be62f2350daa0e923f9cfe7a0161f7c87fd67
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-11 08:26:46 +0200

    text books

--- src/pages/PageBooks.jsx
+++ src/pages/PageBooks.jsx
@@ -1,24 +1,49 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const booksUrl = 'https://gutendex.com/books/?search=nietzsche';
+const booksUrl = 'https://gutendex.com/books/?search=berlin';
+
+// interface
+const book = {
+	title: 'ttt',
+	author: 'aaa',
+	readUrl: 'uuu',
+	imageUrl: 'uuu',
+};
 
 export const PageBooks = () => {
-	const [bookData, setBookData] = useState({ count: 0 });
+	const [books, setBooks] = useState([]);
+
+	const createBooks = (bookData) => {
+		const _books = [];
+		_books.push({ title: 'Test Book #1' }, {title:"Second Book"});
+		setBooks(_books);
+	};
 
 	useEffect(() => {
 		(async () => {
 			const response = await axios(booksUrl);
-			const _bookData = response.data;
-			setBookData(_bookData);
+			const bookData = response.data;
+			createBooks(bookData);
 		})();
 	}, []);
 
 	return (
 		<div className="page_books">
 			<h2>Books</h2>
-			{bookData.count > 0 ? (
-				<p>There are {bookData.count} books.</p>
+			{books.length > 0 ? (
+				<>
+					<div className="books">
+						<p>There are {books.length} books.</p>
+						{books.map((book, index) => {
+							return (
+								<div key={index} className="book">
+									<div className="title">{book.title}</div>
+								</div>
+							);
+						})}
+					</div>
+				</>
 			) : (
 				<div className="waitText">Loading...</div>
 			)}
